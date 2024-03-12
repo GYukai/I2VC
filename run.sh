aa6fecc6bddd7d9550a701a2827bc715295f3e74
guba <yukai.gu@outlook.com>
2024-03-12 11:25:26 +0800

    run.sh: Change the run.sh to use pretrained weight

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ export PYTHONPATH=$PYTHONPATH:$ROOT
 mkdir $ROOT/snapshot
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
         $ROOT/subnet/main.py --log log.txt --config $ROOT/config256.json \
-         --from_scratch
-        # --pretrain $ROOT/snapshot/iter484590.model 
+         --pretrain $ROOT/snapshot/lpips0.03.model
