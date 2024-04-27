9e5df36b60c76e0cae9eac931a165e205051b8cf
guba <yukai.gu@outlook.com>
2024-04-27 23:22:19 +0800

    MOV_test_3090

--- scripts/test_3090.sh
+++ scripts/test_3090.sh
@@ -1,7 +1,11 @@
 source /root/anaconda3/bin/activate gyk
 ROOT=./
 export PYTHONPATH=$PYTHONPATH:$ROOT
-python -u $ROOT/subnet/main.py --log loguvg.txt --testuvg --config config2048.json --pretrain $ROOT/snapshot/iter121149.model
+python -u $ROOT/subnet/main.py --log loguvg.txt --testuvg --config config2048.json \
+ --test-lmd 256 \
+ --pretrain snapshot/FT_0.03_512_2/iter84000.model \
+ --test-path data/kodak/inputs
+
 # CUDA_VISIBLE_DEVICES=3 python -u $ROOT/subnet/main.py --log loguvg.txt --testuvg --config config2048.json --pretrain $ROOT/snapshot/iter565355.model
 # CUDA_VISIBLE_DEVICES=3 python -u $ROOT/subnet/main.py --log loguvg.txt --testuvg --config config2048.json --pretrain $ROOT/snapshot/iter646120.model
 # CUDA_VISIBLE_DEVICES=0  python -u $ROOT/subnet/main.py --log loguvg.txt --testuvg --config config512.json \
