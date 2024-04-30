0bbc1ace807f10ff45781f96140c6c9570d038d3
guba <yukai.gu@outlook.com>
2024-05-01 02:02:23 +0800

    default run-interval to 5000

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ mkdir $ROOT/snapshot
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
         $ROOT/subnet/main.py --log log.txt --config $ROOT/config256.json --mse_loss-factor 1.0 --lps_loss-factor 0.05 \
         --lmd-mode random --lmd-lower_bound 8 --lmd-upper_bound 256 \
-        --test-interval 10 \
+        --test-interval 5000 \
         --exp-name SAMPLE_NAME \
         --batch-per-gpu 2 \
         --test-dataset-path data/Kodak24/ \
