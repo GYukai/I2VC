e886cce77a1f6c3bd248b25bec8beb679e4fba75
cc <sample@foxmail.com>
2024-05-18 00:41:07 +0800

    Update sample run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,15 @@
 ROOT=./
 export PYTHONPATH=$PYTHONPATH:$ROOT
 mkdir $ROOT/snapshot
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch \
         $ROOT/subnet/main.py --log log.txt --config $ROOT/config256.json --mse_loss-factor 1.0 --lps_loss-factor 0.05 \
         --lmd-mode random --lmd-lower_bound 8 --lmd-upper_bound 256 \
-        --test-interval 5000 \
-        --exp-name SAMPLE_NAME \
+        --test-interval 1500 \
+        --exp-name A800_0506_revised \
         --batch-per-gpu 2 \
         --test-dataset-path data/Kodak24/ \
         --from_scratch
+        # --pretrain snapshot_A/A800_0505_revised/iter4.model
+        # --testuvg
+        # --pretrain snapshot_A/A800_0505_revised/iter4.model
 #        --pretrain snapshot/archive/bpp_mse_iter164000.model
