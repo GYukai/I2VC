d58baed1bdeaeb8cb841c9f0a9aa3a7e193fdab8
cc <sample@foxmail.com>
2024-05-22 18:42:32 +0800

    Delete some unnecessary files

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,11 +6,8 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch \
         $ROOT/subnet/main.py --log log.txt --config $ROOT/config256.json --mse_loss-factor 1.0 --lps_loss-factor 0.05 \
         --lmd-mode random --lmd-lower_bound 8 --lmd-upper_bound 256 \
         --test-interval 1500 \
-        --exp-name A800_0506_revised \
+        --exp-name SAMPLE_NAME \
         --batch-per-gpu 2 \
         --test-dataset-path data/Kodak24/ \
         --from_scratch
-        # --pretrain snapshot_A/A800_0505_revised/iter4.model
-        # --testuvg
-        # --pretrain snapshot_A/A800_0505_revised/iter4.model
-#        --pretrain snapshot/archive/bpp_mse_iter164000.model
+
--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ mkdir $ROOT/snapshot
 accelerate launch --main_process_port 29501 --config_file config_single.yaml   \
         $ROOT/subnet/main.py --log log.txt --config $ROOT/config256.json --mse_loss-factor 0 --lps_loss-factor 1.0 \
         --lmd-mode random --lmd-lower_bound 2 --lmd-upper_bound 16 \
-        --exp-name test \
+        --exp-name TEST \
         --batch-per-gpu 2 \
         --test-path data/Kodak24/kodak \
         --pretrain  snapshot/mark/lpips0.03.model \
